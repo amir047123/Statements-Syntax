074cdd38e0607d85e228ba820b7581124d3eb7fe
amir047123 <amirfaysal047123@gmail.com>
2025-01-29 23:17:51 +0600

    done

---
+++ Statement.js
@@ -0,0 +1,89 @@
+// let x = 10;
+// x = x + 5;
+
+// console.log(x);
+
+// If else Statement
+// let age = 17;
+
+// if (age >= 18) {
+//   console.log("You are a adult");
+// } else {
+//   console.log("You are not a adult");
+// }
+
+// Loop Statement
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("number", i);
+// }
+
+// fuctional statement
+
+// function greet(name) {
+//   console.log("Hello", name);
+// }
+
+// greet("Amir Faysal");
+
+// Switch Statement
+
+// let day = "Saturday";
+
+// switch (day) {
+//   case "Saturday":
+//     console.log("Weekend!");
+//     break;
+//   case "Sunday":
+//     console.log("Holiday!");
+//     break;
+//   default:
+//     console.log("Regular day.");
+// }
+
+// try and catch statement
+// try {
+//   let result = 10 / 0; // division by zero
+//   console.log(result);
+// } catch (error) {
+//   console.log("Something Went Wrong", error);
+// }
+
+// javascript statements
+
+// variable  declaration  // let x = 10;
+// expression // let x = 10 + 5;
+// Assingment // let x = 10;
+// Conditional (if else)
+// //  if (10>=18){}
+
+// loop (for, while, do while, for in, for of)
+// for (let i = 1; i <= 10; i++) {
+//     console.log("number", i);}
+
+// function
+
+// fuction greet(name) {
+//     console.log("Hello", name);
+// }
+// Switch
+
+// switch (day) {
+//     case "Saturday":
+//         console.log("Weekend!");
+//         break;
+//     case "Sunday":
+//         console.log("Holiday!");
+//         break;
+//     default:
+//         console.log("Regular day.");
+// }
+
+// try and catch
+
+// try {
+//     let result = 10 / 0; // division by zero
+//     console.log(result);
+// } catch (error) {
+//     console.log("Something Went Wrong", error);
+// }
---
+++ Syntax.js
@@ -0,0 +1,49 @@
+// let name = "Amir Faysal";
+// const age = 25;
+// var city = " sirajgonj";
+
+// Data Type
+
+// String; // "Amir Faysal"
+// Number; // 25
+// Boolean; // true or false
+// Null; // null
+
+// Array; // [1,2,3,4,5] ["Amir", "Faysal"]
+// Object; // {name: "Amir", age: 25}
+
+// Operators;
+
+// Arithmetic Operator
+
+// let x = 10;
+// let y = 5;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+
+// console.log(x / y);
+// console.log(x % y);
+
+// Comparison Operators
+
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(10 == 5);
+// console.log(10 === 10);
+// console.log(10 != 5);
+
+// Logical Operators
+
+// AND;
+
+// console.log(false && true);
+
+// OR
+
+// console.log(true || false);
+
+// Not Operator
+
+// console.log(!false);
